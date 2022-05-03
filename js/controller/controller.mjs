75bd52b51233b8d719660a90f0565561113704c4
hyemin.shin <hyemin.shin@bespinglobal.com>
2022-05-03 16:22:33 +0900

    주석 정리

--- js/controller/controller.mjs
+++ js/controller/controller.mjs
@@ -1,34 +1,3 @@
-// class getGeo{
-//   handleGeoSucc(position) {
-//     /* Success Handle */
-//     console.log(position);
-//     const latitude = position.coords.latitude;  // 경도
-//     const longitude = position.coords.longitude;  // 위도
-//     const coordsObj = {
-//       latitude,
-//       longitude
-//     }
-//   }
-//   handleGeoErr(err) {
-//     /* Error Handle */
-//     console.log("geo err! " + err);
-//   }
-//   requestCoords() {
-//     navigator.geolocation.getCurrentPosition(this.handleGeoSucc, this.handleGeoErr);
-//   }
-// }
-// function handleGeoSucc(position) {
-//   /* Success Handle */
-//   console.log(position);
-//   const latitude = position.coords.latitude;  // 경도
-//   const longitude = position.coords.longitude;  // 위도
-//   getWeather(latitude,longitude)
-// }
-//
-// function handleGeoErr(err) {
-//   /* Error Handle */
-//   console.log("geo err! " + err);
-// }
 import {getWeather} from "../service/service.mjs";
 let get_weather=new getWeather();
 export class controller{

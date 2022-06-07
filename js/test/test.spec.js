c3e4316df18f9e7d472b80e12ba9bfac42694b58
hyemin.shin <hyemin.shin@bespinglobal.com>
2022-06-07 14:18:55 +0900

    유닛테스트

--- js/test/test.spec.js
+++ js/test/test.spec.js
@@ -1,45 +1,52 @@
-import {initializeApp} from "firebase/app";
+import {getWeather,saveWeather} from "../service/service.mjs";
+// import {weatherAPI} from "../../apikey.js";
+import data from "../../test.json"
+const nock = require('nock')
 
-const sayHello = require('./test').sayHello;
-import {getDatabase, onValue, ref, set} from "firebase/database";
-const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);
-import {firebaseConfig} from "../../apikey";
-const request = require('request')
-const expect = require('chai').expect;
-
-// describe('App test!', function () {
-//   it('sayHello should return hello', function (done) {
-//     if (sayHello() === 'hello') {
-//       done();
-//     }
-//   });
-// });
-
-describe('Firebase test!', function () {
-    it('readData', function (done) {
-      const db=getDatabase();
-      onValue(ref(db,'weather/'),snapshot => {
-        if(snapshot.val()!=undefined){
-          done();
-        }
+// import {initializeApp} from "firebase/app";
+// import {getDatabase, onValue, ref, set} from "firebase/database";
+// const app = initializeApp(firebaseConfig);
+// const database = getDatabase(app);
+// import {firebaseConfig} from "../../apikey";
+import fetch from "node-fetch";
+globalThis.fetch = fetch
+let test=new getWeather();
+let test2=new saveWeather();
+describe('unit test 1',function() {
+  before(function(){
+    nock('https://api.openweathermap.org/data/2.5/weather?lat=36.7796&lon=127.1376&appid=bdab0099b7b556d38af96f7adcc089f3&units=metric')
+      .get('')
+      .reply(200,data);
+  });
+  it('현재 날씨 가져오기', function (done) {
+    test.getWeatherAPI(36.7796,127.1376).then(()=>
+      done())
+      .catch(err=>{
+        done(err)
+        console.log('err:'+err)
       })
-  });
-});
-describe('Api test1!', function () {
-  it('getWeather', function (done) {
-    request.get(`https://api.openweathermap.org/data/2.5/weather?lat=36.7796&lon=127.1376&appid=bdab0099b7b556d38af96f7adcc089f3&units=metric`,function (err,res,body){
-      expect(res.statusCode).to.equal(200)
-      done()
     })
-  });
+  after(function(){
+    nock.cleanAll()
+  })
 });
-describe('Api test2!', function () {
-  it('getWeekWeather', function (done) {
-    request.get(`https://api.openweathermap.org/data/2.5/onecall?lat=36.7796&lon=127.1376&exclude=current,minutely,alerts&appid=bdab0099b7b556d38af96f7adcc089f3&units=metric`,function (err,res,body){
-      expect(res.statusCode).to.equal(200)
-      done()
-    })
-  });
-});
+// describe('unit test 2', function () {
+//   it('일주일 날씨 가져오기', function (done) {
+//     test.getWeekWeatherAPI(36.7796, 127.1376).then(() =>
+//       done())
+//       .catch((err) => {
+//         console.log('err:' + err)
+//       })
+//   })
+// })
+// describe('Firebase test!', function () {
+//   it('저장된 데이터 확인', function (done) {
+//     test2.readData().then(() =>
+//       done())
+//       .catch((err) => {
+//         console.log('err:' + err)
+//       })
+//   })
+// })
+
 
